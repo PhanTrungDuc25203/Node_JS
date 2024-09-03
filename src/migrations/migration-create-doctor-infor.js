34b7ded0b132be8a1aa4e62a33f1cde4a85d15e3
PhanTrungDuc <phantrungduc2522005@gmail.com>
2024-09-03 16:25:21 +0700

    tạo thêm DB mới lưu chi tiết bác sĩ

---
+++ src/migrations/migration-create-doctor-infor.js
@@ -0,0 +1,60 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        // tên bảng viết hoa hay thường thì mysql tự chuyển thành thường
+        await queryInterface.createTable('Doctor_infor', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+
+            doctorId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            priceId: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            provinceId: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            paymentId: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            clinicAddress: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            clinicName: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            note: {
+                type: Sequelize.STRING
+            },
+            count: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                defaultValue: 0,
+            },
+
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('Doctor_infor');
+    }
+};
